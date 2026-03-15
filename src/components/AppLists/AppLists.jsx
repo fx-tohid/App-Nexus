dffcd7a346f4c2dcee1500e4ee344958e552d732
kamrulhsuzon <nazmulnayun7@gmail.com>
2026-03-16 04:57:00 +0600

    Added search functionality in app list section

--- src/components/AppLists/AppLists.jsx
+++ src/components/AppLists/AppLists.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
 import TrendingApp from '../TrendingApp/TrendingApp';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const AppLists = () => {
 
     const apps = useLoaderData();
+    const [search, setSearch] = useState('');
+    const [filteredApps, setFilteredApps] = useState([]);
+    const handleSearch = e => {
+        console.log(e.target.value);
+        setSearch(e.target.value)
 
+    }
+    useEffect(() => {
+        const results = apps.filter(app => app.title.toLowerCase().includes(search.toLowerCase()));
+        setFilteredApps(results)
+    }, [search, apps])
+
+    
     return (
         <section className="w-full bg-[#fcfdfe] py-16 px-6">
             <div className="max-w-7xl mx-auto">
@@ -17,6 +31,8 @@ const AppLists = () => {
 
                 <div className='flex justify-between my-8'>
                     <h2 className='text-black text-3xl font-medium'>({apps.length}) Apps Found</h2>
+
+                    {/* Search input */}
                     <label className="input ">
                         <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <g
@@ -30,12 +46,12 @@ const AppLists = () => {
                                 <path d="m21 21-4.3-4.3"></path>
                             </g>
                         </svg>
-                        <input name='search' type="search" required placeholder="Search Apps" />
+                        <input value={search} onChange={handleSearch} name='search' type="search" required placeholder="Search Apps" />
                     </label>
                 </div>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-                    {apps.map((app) => <TrendingApp key={app.id} app={app} />)}
+                    {filteredApps.map((app) => <TrendingApp key={app.id} app={app} />)}
                 </div>
 
 
